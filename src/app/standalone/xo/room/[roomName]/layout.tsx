e6e9dcbc2362866ec027efc46fb5a30f738878c2
leswell <simofilevente+git@gmail.com>
2026-02-01 00:03:37 +0200

    fix(room-layout): adjust height to be responsive

--- src/app/standalone/xo/room/[roomName]/layout.tsx
+++ src/app/standalone/xo/room/[roomName]/layout.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import { PropsWithChildren } from "react";
 import { RoomLayout as RoomLayoutComponent } from "@/packages/ui-patterns/layout/room-layout";
+import { useBreakpointValue } from "@chakra-ui/react";
 
 export default function RoomLayout({ children }: PropsWithChildren) {
+    const roomHeight = useBreakpointValue({ 
+        base: "calc(100vh - 8vh)", 
+        md: "calc(100vh - 16vh)" 
+    });
+    
     return (
-        <RoomLayoutComponent initialLayout="split" w="100%" h="calc(100vh - 16vh)" >
+        <RoomLayoutComponent initialLayout="split" w="100%" h={roomHeight} >
             {children}
         </RoomLayoutComponent>
     );
