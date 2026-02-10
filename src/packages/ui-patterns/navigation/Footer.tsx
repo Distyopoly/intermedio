d631278f6441d62c3242b2f0b41c874346f54951
leswell <simofilevente+git@gmail.com>
2026-02-10 20:58:44 +0200

    feat(footer): documentation link added for developers

--- src/packages/ui-patterns/navigation/Footer.tsx
+++ src/packages/ui-patterns/navigation/Footer.tsx
@@ -1,4 +1,4 @@
-import { HStack } from "@chakra-ui/react";
+import { HStack, Link } from "@chakra-ui/react";
 import { ComponentProps } from "react";
 import { ColorModeButton } from "@/packages/ui-components/chakra/color-mode";
 
@@ -10,7 +10,7 @@ export function Footer({ ...props }: Props) {
         <footer>
                 <HStack position="fixed" bottom="0"
                 alignItems="center" justifyContent="center" w="full" gap="3" {...props}>
-                    <p>Version 1.0.0</p>
+                    <Link href={process.env.NEXT_PUBLIC_DOCS_PAGE!} target="_blank">For Developers</Link>
                     <ColorModeButton variant="ghost" opacity={0.3}/>
                     <p>© 2026 Distyopoly</p>
                 </HStack>
