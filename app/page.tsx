68ca5c3e8239015b058eb623b76d59583adbc730
leswell <simofilevente+git@gmail.com>
2026-03-02 00:10:12 +0200

    feat!(standalone): remove standalone page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <Center>
       <HStack mt="30vh" gap={5}>
         <LinkButton href="/platform" variant="solid" size="xl">Platform</LinkButton>
-        <LinkButton href="/standalone" variant="subtle" size="xl">Standalone</LinkButton>
+        {/* <LinkButton href="/standalone" variant="subtle" size="xl">Standalone</LinkButton> */}
       </HStack>
     </Center>
   );
