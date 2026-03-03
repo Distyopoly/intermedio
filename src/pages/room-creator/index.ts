0c42e0af06d26354b66138df26e564f0ed51aeca
leswell <simofilevente+git@gmail.com>
2026-03-03 17:12:31 +0200

    refactor(room-creator): add page

---
+++ src/pages/room-creator/index.ts
@@ -0,0 +1,1 @@
+export { RoomCreatorPage } from "./ui/page";
---
+++ src/pages/room-creator/ui/page.tsx
@@ -0,0 +1,30 @@
+import { AbsoluteCenter, Heading, VStack, Highlight } from "@chakra-ui/react";
+import { CreateRoomButton, RoomDraftProvider, SettingsDialog } from "@features/manage-room";
+
+export function RoomCreatorPage() {
+    return (
+        <AbsoluteCenter>
+            <VStack gap="5em">
+                <Heading userSelect="none" fontSize="6xl" letterSpacing="wide" fontWeight="bold" textAlign="center" lineHeight="shorter" textWrap="balance">
+                    <Highlight query="gamenight" styles={{ color: "red.600" }}>
+                        Start your gamenight
+                    </Highlight>
+                </Heading>
+                <VStack
+                    borderRadius="lg"
+                    layerStyle="fill.subtle"
+                    opacity={0.9}
+                    h="fit-content"
+                    gap={10}
+                    w="fit-content"
+                    padding={10}
+                >
+                    <RoomDraftProvider>
+                        <SettingsDialog variant="ghost" size="2xl" />
+                        <CreateRoomButton roomPrefix="/platform/room" />
+                    </RoomDraftProvider>
+                </VStack>
+            </VStack>
+        </AbsoluteCenter>
+    );
+}
