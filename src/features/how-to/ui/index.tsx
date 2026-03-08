46018d84aa92168aa6d5cbd47a85660c26914276
leswell <simofilevente+git@gmail.com>
2026-03-08 21:31:08 +0200

    feat(how-to): relabel as playbook

--- src/features/how-to/ui/index.tsx
+++ src/features/how-to/ui/index.tsx
@@ -1,8 +1,8 @@
-import { Box, Button, Text } from "@chakra-ui/react";
-import { FaQuestion } from "react-icons/fa";
+import { Button } from "@chakra-ui/react";
 import { ComponentProps } from "react";
 import { DialogBody, DialogCloseTrigger, DialogContent, DialogHeader, DialogRoot, DialogTrigger } from "@/packages/ui-components/dialog";
 import { HowToTabs } from "./tabs";
+import { GiBookmarklet } from "react-icons/gi";
 
 type Props = ComponentProps<typeof Button> & {};
 
@@ -11,7 +11,7 @@ export function HowTo({ ...props }: Props) {
     return (
         <DialogRoot size={{ mdDown: "full", md: "lg" }}>
             <DialogTrigger asChild>
-                <Button size="lg" variant="subtle" aria-label="How to" px={2} {...props}>How to play<FaQuestion /></Button>
+                <Button size="lg" variant="subtle" aria-label="Playbook" px={2} {...props}>Playbook<GiBookmarklet /></Button>
             </DialogTrigger>
             <DialogContent h="50vh">
                 <DialogCloseTrigger />
