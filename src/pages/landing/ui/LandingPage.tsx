31166b83d9ae935d3e222d28900b4da84b01d2c5
leswell <simofilevente+git@gmail.com>
2026-03-10 14:35:41 +0200

    feat(landing page): add margin before content

--- src/pages/landing/ui/LandingPage.tsx
+++ src/pages/landing/ui/LandingPage.tsx
@@ -1,19 +1,21 @@
-import { VStack, Heading, Text, ButtonGroup, HStack } from "@chakra-ui/react";
+import { VStack, Heading, Text, ButtonGroup, HStack, Center, AbsoluteCenter, Bleed } from "@chakra-ui/react";
 import { LinkButton } from "@packages/ui-components/link-button";
 
 export function LandingPage() {
     return (
-        <VStack layerStyle="fill.muted" h="full" gap={8} p={4}>
-            <Heading size="5xl" fontWeight="black" letterSpacing="tight">Welcome to Distyopoly</Heading>
-        
-            <Text fontSize="xl" color="fg.muted" maxW="md">
-                Distyopoly is a place for you and your friends. 
+        // <AbsoluteCenter>
+                <VStack layerStyle="fill.muted" h="full" gap={8} p={4} pt={20}>
+                <Heading size="5xl" fontWeight="black" letterSpacing="tight">Welcome to Distyopoly</Heading>
 
-            </Text>
-            <HStack gap={10} mt={22}>
-              <LinkButton size="xl" href="/tutorial" variant="solid">Start Here</LinkButton>
-              <LinkButton size="xl" href="/create-room" variant="outline">Create room</LinkButton>
-            </HStack>
-        </VStack>
+                <Text fontSize="xl" color="fg.muted" maxW="md">
+                    Distyopoly is a place for you and your friends.
+
+                </Text>
+                <HStack gap={10} mt={22}>
+                    <LinkButton size="xl" href="/tutorial" variant="solid">Start Here</LinkButton>
+                    <LinkButton size="xl" href="/create-room" variant="outline">Create room</LinkButton>
+                </HStack>
+            </VStack>
+        // </AbsoluteCenter>
     );
 }
